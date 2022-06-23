473ea12a05e4522c9c001552b6c1e2d59afa34f0
Achraf Karboul <59778984+achrafkarboul@users.noreply.github.com>
2022-06-23 16:27:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import urllib
 
 #DATABASE_URL = "sqlite:///./test.db"
 
-host_server = os.environ.get('host_server', 'localhost')
+host_server = os.environ.get('host_server', 'akuvitpostgresql.postgres.database.azure.com')
 db_server_port = urllib.parse.quote_plus(str(os.environ.get('db_server_port', '5432')))
-database_name = os.environ.get('database_name', 'fastapi')
-db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'postgres')))
-db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'secret')))
-ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','prefer')))
+database_name = os.environ.get('database_name', 'akuvitBD')
+db_username = urllib.parse.quote_plus(str(os.environ.get('db_username', 'akuvitpostgresql@akuvitpostgresql')))
+db_password = urllib.parse.quote_plus(str(os.environ.get('db_password', 'Achrafkarboul123456')))
+ssl_mode = urllib.parse.quote_plus(str(os.environ.get('ssl_mode','require')))
 DATABASE_URL = 'postgresql://{}:{}@{}:{}/{}?sslmode={}'.format(db_username, db_password, host_server, db_server_port, database_name, ssl_mode)
 
 database = databases.Database(DATABASE_URL)
